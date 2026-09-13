c41faaed4fa4cca132ad9c117033f00a1d64865f
POS Developer <developer@pos-system.local>
2026-09-13 18:11:32 +0100

    Create installable Nemesis Eye PWA

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "nemesis-eye-v2";
+const APP_SHELL = [
+    "./",
+    "./eye_pwa.html",
+    "./manifest.webmanifest",
+    "./icon.svg",
+    "./icon-192.svg",
+    "./icon-512.svg",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_SHELL))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => Promise.all(
+                cacheNames
+                    .filter(cacheName => cacheName !== CACHE_NAME)
+                    .map(cacheName => caches.delete(cacheName))
+            ))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => cachedResponse || fetch(event.request))
+    );
+});
